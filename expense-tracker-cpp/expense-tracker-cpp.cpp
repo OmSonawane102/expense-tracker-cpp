86c12c8e779732ebdec56c3577098e4bc5c25ef4
OmSonawane102 <omsonawane1020@gmail.com>
2026-08-14 23:47:25 +0530

    Add project files.

---
+++ expense-tracker-cpp/expense-tracker-cpp.cpp
@@ -0,0 +1,37 @@
+﻿// expense-tracker-cpp.cpp : Defines the entry point for the application.
+//
+
+#include "expense-tracker-cpp.h"
+
+#include <iostream>
+
+void clearConsole() {
+#ifdef _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
+
+int main() {
+
+	char input_option;
+	std::string version = "PROTOTYPE - 01";
+
+	// Basic ANSI escape codes
+	const std::string RESET = "\033[0m";
+	const std::string BOLD = "\033[1m";
+
+	while (true) {
+		clearConsole(); // clear screen after each iteration 
+
+		std::cout << BOLD << "Expense Tracker CLI - " << RESET << version << std::endl;
+		std::cout << "------------------------------------------------------------" << std::endl << std::endl;
+
+		std::cout << "[a]dd transaction" << std::endl;
+		std::cout <<
+
+			std::cin.get();
+	}
+}
+
